964e2591b4ae3c9b964db46658b11b32ab807756
oreapatricia-cyber <oreapatricia@gmail.com>
2026-03-29 19:39:31 -0400

    adding the core

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ function generatePoem(event) {
   event.preventDefault();
   let poemInputElement = document.querySelector("#poem-input");
   poemelElement.innerHtml = "Generating poem...";
+  new Typewriter("#poem-display", {
+    strings: "Cuántas veces, amor, te amé sin verte y tal vez sin recuerdo,",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
 }
 
 let poemFormElement = document.querySelector("#poem-form");
