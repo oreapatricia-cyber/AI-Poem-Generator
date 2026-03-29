ba15c9168a3781abd8139da8564456526b89d719
oreapatricia-cyber <oreapatricia@gmail.com>
2026-03-29 19:58:37 -0400

    updating

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ function displayPoem(response) {
     cursor: "",
   });
 }
+function generatePoem(event) {
+  event.preventDefault();
+  let ApiKey = "94bo483a37241b0t66efb57f3864046e";
+  let APiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
+}
 
 let poemFormElement = document.querySelector("#poem-form");
 poemFormElement.addEventListener("submit", generatePoem);
