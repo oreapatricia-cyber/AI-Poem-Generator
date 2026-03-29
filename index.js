ad08aa8948fe1d4deb9b7edae6e13ed00d942745
oreapatricia-cyber <oreapatricia@gmail.com>
2026-03-29 19:59:14 -0400

    apikey

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function displayPoem(response) {
 function generatePoem(event) {
   event.preventDefault();
   let ApiKey = "94bo483a37241b0t66efb57f3864046e";
-  let APiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
+  let APiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${ApiKey}`;
 }
 
 let poemFormElement = document.querySelector("#poem-form");
