daf235b2647dd16ef9bf2ca96343d979fe21f96c
oreapatricia-cyber <oreapatricia@gmail.com>
2026-03-29 19:52:16 -0400

    adding API Key

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function displayPoem(response) {
     new Typewriter("#poem-display", {
-    strings: "Cuántas veces, amor, te amé sin verte y tal vez sin recuerdo,",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
@@ -8,6 +8,7 @@ function displayPoem(response) {
 
 function generatePoem(event) {
   event.preventDefault();
+  let apikey="94bo483a37241b0t66efb57f3864046e";
   let poemInputElement = document.querySelector("#poem-input");
   poemelElement.innerHtml = "Generating poem...";
   });
