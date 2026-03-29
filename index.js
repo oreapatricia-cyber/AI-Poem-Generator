59fb3b5460ce16680419962cf3e01471448f1b2f
oreapatricia-cyber <oreapatricia@gmail.com>
2026-03-29 19:56:12 -0400

    cleaning

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 function displayPoem(response) {
-    new Typewriter("#poem-display", {
+  new Typewriter("#poem-display", {
     strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
-}
-
-function generatePoem(event) {
-  event.preventDefault();
-  let apikey="94bo483a37241b0t66efb57f3864046e";
-  let ApiUrl=`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`
-  let poemInputElement = document.querySelector("#poem-input");
-  poemelElement.innerHtml = "Generating poem...";
   });
 }
 
