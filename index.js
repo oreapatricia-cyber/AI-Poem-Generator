4409a125daaa9712ef97ccbf017b26ee4a49085f
oreapatricia-cyber <oreapatricia@gmail.com>
2026-03-29 19:54:21 -0400

    typo

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function displayPoem(response) {
 function generatePoem(event) {
   event.preventDefault();
   let apikey="94bo483a37241b0t66efb57f3864046e";
-  Let ApiUrl=`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`
+  let ApiUrl=`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`
   let poemInputElement = document.querySelector("#poem-input");
   poemelElement.innerHtml = "Generating poem...";
   });
