aa8f4ac9e3fcb8eabcb8c999e4372708c8c8707c
oreapatricia-cyber <oreapatricia@gmail.com>
2026-03-30 19:06:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function displayPoem(response) {
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
-    delay: 1,
+    delay: 20,
   });
 
   document.querySelector("#loading").style.display = "none";
@@ -14,9 +14,9 @@ function displayPoem(response) {
 
 function generatePoem(event) {
   let ApiKey = "94bo483a37241b0t66efb57f3864046e";
-  let prompt = "Generate a poems in English.";
+  let prompt = "Generate poems in English.";
   let context =
-    "You are a poet expert who writes beautiful poems about love in English. The poem should be romantic and emotional, capturing the essence of love in a heartfelt way, specially from great poets of Latin America. Use vivid imagery and expressive language to evoke strong emotions in the reader. The poem should be concise yet powerful, leaving a lasting impression on the audience. The tone should be passionate and sincere, conveying the intensity of love in a way that resonates with the reader.Make it shorter than 100 words.";
+    "You are a poet expert who writes beautiful poems in English. The poem should be romantic and emotional, capturing the essence of love in a heartfelt way, specially from great poets of Latin America. Use vivid imagery and expressive language to evoke strong emotions in the reader. The poem should be concise yet powerful, leaving a lasting impression on the audience. The tone should be passionate and sincere, conveying the intensity of love in a way that resonates with the reader.Make it shorter than 100 words.";
 
   let ApiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${ApiKey}`;
   axios.get(ApiUrl).then(displayPoem);
