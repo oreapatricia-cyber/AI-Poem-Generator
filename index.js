45bc66393b51942d0359b0e772f4bb6ceac34d62
oreapatricia-cyber <oreapatricia@gmail.com>
2026-03-29 21:56:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function displayPoem(response) {
 
 function generatePoem(event) {
   let ApiKey = "94bo483a37241b0t66efb57f3864046e";
-  let prompt = "";
+  let prompt = "Generate a poem about love in Spanish.";
   let context = "";
 
   let ApiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${ApiKey}`;
