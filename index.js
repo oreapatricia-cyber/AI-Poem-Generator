500524ee96c223754cc512725572279ea9ab702f
oreapatricia-cyber <oreapatricia@gmail.com>
2026-03-29 20:06:43 -0400

    adding context

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ function generatePoem(event) {
   event.preventDefault();
   let ApiKey = "94bo483a37241b0t66efb57f3864046e";
   let prompt = "Generate a Spanish poem about Love";
-  let context = "";
+  let context =
+    "You are a poet who writes beautiful poems about love in Spanish.";
   let APiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${ApiKey}`;
   axios.get(APiUrl).then(displayPoem);
 }
