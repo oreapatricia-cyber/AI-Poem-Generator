f6488a1f04a9b217013738b7110e159617043c89
oreapatricia-cyber <oreapatricia@gmail.com>
2026-03-29 21:12:00 -0400

    1

--- index.js
+++
@@ -1,6 +0,0 @@
-function generatePoem(event) {
-  event.preventDefault();
-}
-
-let poemformElement = document.QuerySelector("#poem-generator-form");
-poemFormElement.addEventlistener("submit", generatePoem);
