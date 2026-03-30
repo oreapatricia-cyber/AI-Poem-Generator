b9937d8a6dce0fba2b5ebbbe4d0af91d5ecb6229
oreapatricia-cyber <oreapatricia@gmail.com>
2026-03-30 19:04:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function displayPoem(response) {
 
 function generatePoem(event) {
   let ApiKey = "94bo483a37241b0t66efb57f3864046e";
-  let prompt = "Generate a poem about love in Spanish.";
+  let prompt = "Generate a poems in English.";
   let context =
     "You are a poet expert who writes beautiful poems about love in English. The poem should be romantic and emotional, capturing the essence of love in a heartfelt way, specially from great poets of Latin America. Use vivid imagery and expressive language to evoke strong emotions in the reader. The poem should be concise yet powerful, leaving a lasting impression on the audience. The tone should be passionate and sincere, conveying the intensity of love in a way that resonates with the reader.Make it shorter than 100 words.";
 
