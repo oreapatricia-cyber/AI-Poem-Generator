488118d755ab13da1bce4ba75dd3fcc134e8835c
oreapatricia-cyber <oreapatricia@gmail.com>
2026-03-29 21:50:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+function displayPoem(response) {}
+
 function generatePoem(event) {
   let ApiKey = "94bo483a37241b0t66efb57f3864046e";
   let prompt = "";
@@ -5,6 +7,7 @@ function generatePoem(event) {
 
   let ApiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${ApiKey}`;
 
+  axios.get(ApiUrl).then(displayPoem);
   event.preventDefault();
   new Typewriter("#poem", {
     strings: "Cuántas veces, amor, te amé sin verte y tal vez sin recuerdo,",
